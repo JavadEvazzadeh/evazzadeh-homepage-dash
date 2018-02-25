26358a0a74822319cc8bdddadc0ca4500b9aeeef
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-02-25 03:59:30 +0330

    update content

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -7,10 +7,10 @@ class twigTransTerms
 	//-------------------------------------------------------content\home\display.html
 	echo T_("Home");                                                                  // Line 17
 	echo T_("Slogan");                                                                // Line 18
-	echo T_("About");                                                                 // Line 182
+	echo T_("About");                                                                 // Line 216
 	echo T_("Contact");                                                               // Line 20
-	echo T_("Check on Github");                                                       // Line 151
-	echo T_("Download Latest Version");                                               // Line 150
+	echo T_("Check on Github");                                                       // Line 185
+	echo T_("Download Latest Version");                                               // Line 184
 	echo T_("Language List");                                                         // Line 26
 	echo T_("Find Out More");                                                         // Line 39
 	echo T_("Secure, Fast and Flexible");                                             // Line 61
@@ -24,24 +24,30 @@ class twigTransTerms
 	echo T_("Hasan Salehi Hajiabadi");                                                // Line 87
 	echo T_("and");                                                                   // Line 87
 	echo T_("Omid Pilevar");                                                          // Line 87
-	echo T_("2013");                                                                  // Line 97
-	echo T_("Framework is Born");                                                     // Line 98
-	echo T_("Idea of create PHP framework was started");                              // Line 101
-	echo T_("Hasan selected to create framework basics.");                            // Line 101
-	echo T_("November 2014");                                                         // Line 111
-	echo T_("First Service");                                                         // Line 112
-	echo T_("First project is launching with this framework");                        // Line 115
-	echo T_("September 2015");                                                        // Line 125
-	echo T_("Ermile");                                                                // Line 183
-	echo T_("We can doing hard job!");                                                // Line 129
-	echo T_("We can register Ermile trademark:)");                                    // Line 129
-	echo T_("To be <br />continue...");                                               // Line 135
-	echo T_("Location");                                                              // Line 170
-	echo T_("Qom, IRAN");                                                             // Line 171
-	echo T_("+98 25 3650 5281");                                                      // Line 171
-	echo T_("Another Languages");                                                     // Line 174
-	echo T_(" is a free to use, open source PHP framework created by");               // Line 183
-	echo T_("Copyright");                                                             // Line 192
+	echo T_("2013");                                                                  // Line 98
+	echo T_("Framework is Born");                                                     // Line 99
+	echo T_("Idea of create PHP framework was started");                              // Line 102
+	echo T_("Hasan selected to create framework basics.");                            // Line 102
+	echo T_("November 2014");                                                         // Line 113
+	echo T_("First Service");                                                         // Line 114
+	echo T_("First project is launching with this framework");                        // Line 117
+	echo T_("September 2015");                                                        // Line 128
+	echo T_("Ermile");                                                                // Line 217
+	echo T_("We can doing hard job!");                                                // Line 132
+	echo T_("We can register Ermile trademark:)");                                    // Line 132
+	echo T_("March 2017");                                                            // Line 143
+	echo T_("Sarshomar Launched");                                                    // Line 144
+	echo T_("Our first public, world class and enterprise project is released.");     // Line 147
+	echo T_("September 2017");                                                        // Line 158
+	echo T_("Rename to Dash");                                                        // Line 159
+	echo T_("We decide to combine Saloos and some other subprojects to Dash!");       // Line 162
+	echo T_("To be <br />continue...");                                               // Line 169
+	echo T_("Location");                                                              // Line 204
+	echo T_("Qom, IRAN");                                                             // Line 205
+	echo T_("+98 25 3650 5281");                                                      // Line 205
+	echo T_("Another Languages");                                                     // Line 208
+	echo T_(" is a free to use, open source PHP framework created by");               // Line 217
+	echo T_("Copyright");                                                             // Line 226
 
 	//--------------------------------------------includes\languages\trans_static.html
 	echo T_("Dash");                                                                  // Line 7 Seperate
