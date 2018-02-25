3f14b2cc3b413ec489869019926b9993bf470725
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-02-25 03:32:41 +0330

    fix links, update trans and some other fix

--- includes/languages/trans_twig.php
+++ includes/languages/trans_twig.php
@@ -1,6 +1,53 @@
 <?php
-private function transtext()
+class twigTransTerms
 {
+ private function transtext()
+ {
 
+	//-------------------------------------------------------content\home\display.html
+	echo T_("Home");                                                                  // Line 17
+	echo T_("Slogan");                                                                // Line 18
+	echo T_("About");                                                                 // Line 182
+	echo T_("Contact");                                                               // Line 20
+	echo T_("Check on Github");                                                       // Line 151
+	echo T_("Download Latest Version");                                               // Line 150
+	echo T_("Language List");                                                         // Line 26
+	echo T_("Find Out More");                                                         // Line 39
+	echo T_("Secure, Fast and Flexible");                                             // Line 61
+	echo T_("Our story in timeline");                                                 // Line 71
+	echo T_("2006-2010");                                                             // Line 83
+	echo T_("Our Friendship Beginnings");                                             // Line 84
+	echo T_("We find each other in Iran national skills competition that knowns as"); // Line 87
+	echo T_("Worldskills");                                                           // Line 87
+	echo T_("Javad Evazzadeh Kakroudi");                                              // Line 87
+	echo T_("Saman Soltani");                                                         // Line 87
+	echo T_("Hasan Salehi Hajiabadi");                                                // Line 87
+	echo T_("and");                                                                   // Line 87
+	echo T_("Omid Pilevar");                                                          // Line 87
+	echo T_("2013");                                                                  // Line 97
+	echo T_("Framework is Born");                                                     // Line 98
+	echo T_("Idea of create PHP framework was started");                              // Line 101
+	echo T_("Hasan selected to create framework basics.");                            // Line 101
+	echo T_("November 2014");                                                         // Line 111
+	echo T_("First Service");                                                         // Line 112
+	echo T_("First project is launching with this framework");                        // Line 115
+	echo T_("September 2015");                                                        // Line 125
+	echo T_("Ermile");                                                                // Line 183
+	echo T_("We can doing hard job!");                                                // Line 129
+	echo T_("We can register Ermile trademark:)");                                    // Line 129
+	echo T_("To be <br />continue...");                                               // Line 135
+	echo T_("Location");                                                              // Line 170
+	echo T_("Qom, IRAN");                                                             // Line 171
+	echo T_("+98 25 3650 5281");                                                      // Line 171
+	echo T_("Another Languages");                                                     // Line 174
+	echo T_(" is a free to use, open source PHP framework created by");               // Line 183
+	echo T_("Copyright");                                                             // Line 192
+
+	//--------------------------------------------includes\languages\trans_static.html
+	echo T_("Dash");                                                                  // Line 7 Seperate
+
+	//---------------------public_html\static\siftal\fonts\siftal\icons-reference.html
+
+ }
 }
 ?>
