42bbbd74cb93069f95f0ad3d8c23fc35e6581a68
Reza <rm.biqarar@gmail.com>
2018-04-21 17:20:32 +0430

    fix content view

--- content/view.php
+++ content/view.php
@@ -1,5 +1,5 @@
 <?php
-namespace mvc;
+namespace content;
 
 class view
 {
@@ -9,7 +9,7 @@ class view
 		\dash\data::site_title(T_("Dash"));
 		\dash\data::site_desc(T_("Free PHP Framework & CMS!"));
 		\dash\data::site_slogan(T_("The simple framework for php programmers ;)"));
-		\dash\data::page_desc($this->data->site_desc(). ' | '. \dash\data::site_slogan());
+		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
 		\dash\data::include_css(false);
 		\dash\data::bodyclass(null);
 	}
