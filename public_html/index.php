7e3a6f6b33907c94b6c09dc8394b69c068c09172
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-11-28 05:37:30 +0330

    use new index to route old wp website

--- public_html/index.php
+++ public_html/index.php
@@ -3,17 +3,45 @@
  @ In the name Of Allah
 **/
 
-// if Dash exist, require it else show related error message
-if ( file_exists( '../dash/autoload.php') )
+$myFirst = null;
+if(isset($_SERVER['REQUEST_URI']))
 {
-	require_once( '../dash/autoload.php');
+	$myAddr  = $_SERVER['REQUEST_URI'];
+	$myAddr  = trim($myAddr, '/');
+	$myFirst = strtok($myAddr, '/');
 }
-elseif ( file_exists( '../../dash/autoload.php') )
+
+switch ($myFirst)
 {
-	require_once( '../../dash/autoload.php');
-}
-else
-{   // A config file doesn't exist
-	exit("<p>We can't find <b>Dash</b>! Please contact administrator!</p>");
+	case 'a':
+	case 'cp':
+	case 'su':
+	case 'hook':
+	case 'en':
+	case false:
+		// run dash
+		// if Dash exist, require it else show related error message
+		if ( file_exists( '../dash/autoload.php') )
+		{
+			require_once( '../dash/autoload.php');
+		}
+		elseif ( file_exists( '../../dash/autoload.php') )
+		{
+			require_once( '../../dash/autoload.php');
+		}
+		else
+		{   // A config file doesn't exist
+			exit("<p>We can't find <b>Dash</b>! Please contact administrator!</p>");
+		}
+
+		break;
+
+	default:
+		// run wp
+		define('WP_USE_THEMES', true);
+
+		/** Loads the WordPress Environment and Template */
+		require('./blog/wp-blog-header.php');
+		break;
 }
 ?>
