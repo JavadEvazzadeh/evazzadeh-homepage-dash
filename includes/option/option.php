ec7c0998662c2c4a3b57eff5251935536d56a8c7
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-12-13 01:46:31 +0330

    add google analytics code

--- includes/option/option.php
+++ includes/option/option.php
@@ -22,9 +22,11 @@ self::$url['protocol'] = 'https';
 /**
  * system default lanuage
  */
-self::$config['site']['title']         = "Javad Evazzadeh Kakroudi";
-self::$config['site']['desc']          = "Trust Me, I'm an Engineer | وب شخصی من جایی برای انتشار دانسته های یک انسان. این وب سایت بیشتر جنبه آموزشی داره. پس احتمالا چیزی هم برای شما پیدا میشه!";
-self::$config['site']['slogan']        = "Software Solution Designer";
+self::$config['site']['title']           = "Javad Evazzadeh Kakroudi";
+self::$config['site']['desc']            = "Trust Me, I'm an Engineer | وب شخصی من جایی برای انتشار دانسته های یک انسان. این وب سایت بیشتر جنبه آموزشی داره. پس احتمالا چیزی هم برای شما پیدا میشه!";
+self::$config['site']['slogan']          = "Software Solution Designer";
+self::$config['site']['googleAnalytics'] = "UA-29692827-1";
+
 
 self::$config['debug']                 = true;
 self::$config['enter']['force_verify'] = false;
